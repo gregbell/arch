00b9b517f461d894ad660a6c5880f64b499080e0
Greg Bell <code@gregbell.ca>
2024-08-30 18:24:02 -0700

    Fix missing sudo's for chaotic install

--- install.sh
+++ install.sh
@@ -17,11 +17,11 @@ gum confirm "Have you updated /etc/pacman.conf?" || { echo "Please update /etc/p
 
 echo -e "\n===${RED} Installing the Chaotic AUR${NC}"
 
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo pacman -Syy
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+sudo pacman -Syy --noconfirm
 
 echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
 
