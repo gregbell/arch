0e220348440abff5460350ae538c5d2711289df8
Greg Bell <code@gregbell.ca>
2024-09-07 08:46:53 -0700

    Run install directly in arch chroot

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ "$EUID" -ne 0 ]
+  then echo "This script is expected to be run as root during install."
+  exit
+fi
+
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
@@ -9,36 +14,36 @@ NC='\033[0m' # No Color
 echo -e "${YELLOW}== Arch Install${NC}"
 
 echo -e "\n=== ${RED}Ensure Parallel Downloads in pacman.conf${NC}"
-sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 echo -e "\n=== ${RED}Set a nice TTY font${NC}"
-sudo pacman -S --needed terminus-font
-echo -e 'FONT=ter-122b' | sudo tee -a /etc/vconsole.conf
+pacman -S --needed terminus-font
+echo -e 'FONT=ter-122b' | tee -a /etc/vconsole.conf
 
 echo -e "\n=== ${RED}Check initial deps${NC}"
-sudo pacman -S --needed gum
+pacman -S --needed gum
 
 echo -e "\n=== ${RED}LTS Kernel{NC}"
 for f in /boot/loader/entries/*_linux.conf
 do
-    sudo cp "$f" "${f/_linux.conf/_linux-lts.conf}"
-    sudo sed -i 's|Arch Linux|Arch Linux LTS Kernel|g' "$f"
-    sudo sed -i 's|vmlinuz-linux|vmlinuz-linux-lts|g' "$f"
-    sudo sed -i 's|initramfs-linux.img|initramfs-linux-lts.img|g' "$f"
+    cp "$f" "${f/_linux.conf/_linux-lts.conf}"
+    sed -i 's|Arch Linux|Arch Linux LTS Kernel|g' "$f"
+    sed -i 's|vmlinuz-linux|vmlinuz-linux-lts|g' "$f"
+    sed -i 's|initramfs-linux.img|initramfs-linux-lts.img|g' "$f"
 done
 
 echo -e "\n===${RED} Install the Chaotic AUR${NC}"
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
-echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
-sudo pacman -Syy --noconfirm
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | tee -a /etc/pacman.conf
+pacman -Syy --noconfirm
 
 echo -e "\n===${RED} AUR${NC}"
 
 if ! command -v yay &> /dev/null; then
-    sudo pacman -S --needed --noconfirm yay
+    pacman -S --needed --noconfirm yay
     yay -Syy
 else
     echo "Yay is already installed, moving on."
@@ -60,28 +65,28 @@ selected_files=$(gum choose --no-limit "${package_files[@]}")
 # Loop through the selected files and install the packages
 for file in $selected_files; do
   echo "> Install $file..."
-  sudo pacman -S --needed --noconfirm $(cat "$file")
+  pacman -S --needed --noconfirm $(cat "$file")
 done
 
 echo "> Enable base services"
-sudo systemctl enable --now NetworkManager
+systemctl enable --now NetworkManager
 
 echo "> Install 1Password..."
-sudo pacman -S --needed 1password
+pacman -S --needed 1password
 yay -S --needed 1password-cli
 
 echo -e "\n\n${YELLOW}== Installation complete${NC}"
 
 function setup-tailscale() {
-    sudo pacman -S tailscale
-    sudo systemctl enable --now tailscaled
-    sudo tailscale up
+    pacman -S tailscale
+    systemctl enable --now tailscaled
+    tailscale up
 }
 
 gum confirm "Install and configure tailscale?" && setup-tailscale
 
 function setup-syncthing () {
-    sudo pacman -S syncthing
+    pacman -S syncthing
 
     for user in /home/*
     do
